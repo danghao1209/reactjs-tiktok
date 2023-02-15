a6401fcda3cd0ab31fdeb5e24192e007363883fb
Dang Hao <62649837+haotamht@users.noreply.github.com>
2023-02-15 19:25:50 +0700

    bai113

--- src/layouts/DefaultLayout/Sidebar/index.js
+++ src/layouts/DefaultLayout/Sidebar/index.js
@@ -1,14 +1,1 @@
-import classNames from 'classnames/bind';
-import styles from './Sidebar.module.scss';
-
-const cx = classNames.bind(styles);
-
-function Sidebar() {
-    return (
-        <div className={cx('wrapper')}>
-            <div>Sidebar</div>
-        </div>
-    );
-}
-
-export default Sidebar;
+export { default } from './Sidebar';
